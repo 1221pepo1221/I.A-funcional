0b0457df4e8f3ee9044e225d6e49a44a816453b0
Pedro H R C <106977723+1221pepo1221@users.noreply.github.com>
2024-06-11 16:17:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,28 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas= [
     {
-    enunciado: "Pergunta 1";
-    alternativas:[ "Alternativa 1", "Alternativa 2"
+    enunciado: "qual e a importancia do multicuturismo na sociedade canadense?";
+    alternativas:[ "Promove a coexistência harmoniosa de diferentes culturas e é oficialmente reconhecido pelo governo ", "È uma prática recente e ainda não amplamente aceita na sociedade canadense"
+    ]
+    },
+    {
+    enunciado: "quais as duas linguas oficiais do canada?";
+    alternativas:[ "ingles e espanhol", "ingles e frances"
+    ]
+    },
+    {
+    enunciado: "o que e o fesfival de calgary stampede?";
+    alternativas:[ "Um festival de cinema realizado anualmente em Toronto.", "Um gfestival anual em Calgary que celebra a herança ocidental do Canadá com rodeios e exposições."
+    ]
+    },
+    {
+    enunciado: "a cultura indigena influencia a indentidade do canada?";
+    alternativas:[ "sim.", "não."
+    ]
+    },
+    {
+    enunciado: "o que e comemorado no dia do canada";
+    alternativas:[ "A data da confederação do país em 1867, celebrada com paradas, fogos de artifício e eventos comunitários.", "O fim do inverno, marcado por festividades ao ar livre e esportes de inverno."
     ]
     }
-]
+];
